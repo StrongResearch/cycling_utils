655b4c3959620bc117cd864b3d01e3566f05d9ef
adaadam4qalumfvjdstjpx7zyvlebh2u <adam@strongcompute.com>
2023-10-27 12:47:28 +1100

    lightning made an optional dependency

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
     name="cycling_utils",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["lightning==2.1.0rc0"],
+    install_requires=[
+    ],
+    extras_require={
+        "lightning": ["lightning==2.1.0rc0"]
+    },
 )
