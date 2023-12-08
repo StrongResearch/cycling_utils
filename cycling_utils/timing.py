cbc47deb750dc9d2dc55c0a7639a5a56ad480c5b
adaadam4qalumfvjdstjpx7zyvlebh2u <adam@strongcompute.com>
2023-12-08 05:40:39 +0000

    updated timer to optionally return seconds since last report

--- cycling_utils/timing.py
+++ cycling_utils/timing.py
@@ -1,7 +1,6 @@
 import os
 import time
 
-
 class TimestampedTimer:
 
     """
@@ -22,10 +21,10 @@ class TimestampedTimer:
     """
 
     def __init__(self, report=None, start_time=None, running=0):
+        self.start_time = start_time if start_time is not None else time.time()
+        self.running = running
+        report = report if report else "Start"
         if str(os.environ.get("RANK", "NONE")) in ["0", "NONE"]:
-            self.start_time = start_time if start_time is not None else time.time()
-            self.running = running
-            report = report if report else "Start"
             print(
                 "[ {} ] {:<70}{:>12} ms, {:>12} s total".format(
                     time.strftime("%Y-%m-%d %H:%M:%S"),
@@ -35,11 +34,11 @@ class TimestampedTimer:
                 )
             )
 
-    def report(self, annot):
+    def report(self, annot, ret=False):
+        now = time.time()
+        duration = now - self.start_time
+        self.running += duration
         if str(os.environ.get("RANK", "NONE")) in ["0", "NONE"]:
-            now = time.time()
-            duration = now - self.start_time
-            self.running += duration
             print(
                 "[ {} ] {:<70}{:>12} ms, {:>12} s total".format(
                     time.strftime("%Y-%m-%d %H:%M:%S"),
@@ -48,4 +47,6 @@ class TimestampedTimer:
                     f"{self.running:,.2f}",
                 )
             )
-            self.start_time = now
+        self.start_time = now
+        if ret:
+            return duration
